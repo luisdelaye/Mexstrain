3b08b71c7ca18ccdeeff48f75a1fbb908cbaa1a9
Luis Delaye <luis.delaye@cinvestav.mx>
2021-03-05 14:52:39 -0600

    Update createfiles.pl

--- createfiles.pl
+++ createfiles.pl
@@ -3,9 +3,9 @@
 # This script extracts IDs from a metadata.tsv file 
 
 # use
-# perl createfiles.pl ncov_Mex_IDs.txt gisaid_hcov-19_Mex.e1.fasta sequences.fasta metadata.tsv 
+# perl createfiles.pl ncov_Mex_IDs.txt gisaid_hcov-19_Mex.e1.fasta sequences.fasta metadata.tsv gisaid_hcov-19_Mex.e1.tsv
 
-# out: outfile.fasta, outfile.txt
+# out: outfile.fasta, outfile.txt, outfile2.fasta, outfile2.txt
 
 # See https://github.com/luisdelaye/Mexstrain/ for more details
 
@@ -26,19 +26,23 @@ my $fileNs = $ARGV[0]; # archivo de Nextstrain, p.ej. ncov_Mex_IDs.txt
 my $fileGS = $ARGV[1]; # archivo de GISAID p.ej. gisaid_hcov-19_2020_11_16_01_Mex.e1.fasta
 my $fileSe = $ARGV[2]; # archivo de GISAID p.ej. sequences.fasta
 my $fileMe = $ARGV[3]; # archivo de GISAID p.ej. metadata.tsv
+my $fileM2 = $ARGV[4]; # archivo de GISAID p.ej. gisaid_hcov-19_Mex.e1.tsv
 
 my %nextstrain;
 my %gisaid;
+my %global;
 
 #-----------------------------------------------------------------------------------------
 # Primero guardo los IDs de las secuencias que estan en el archivo de Nextstrain
 
+print ("-----\n");
 my $ns = 0;
 print ("Sequences from Mexico in Nextstrain latest global analysis (file: $fileNs)\n");
 open (MIA, "$fileNs") or die ("Can't open file $fileNs\n");
 while (my $linea = <MIA>){
 	chomp ($linea);
 	$nextstrain{$linea} = 0;
+	$global{$linea} = 0;
 	print ("$nextstrain{$linea}\t($linea)\n");
 	$ns++;
 }
@@ -51,27 +55,16 @@ print ("-----\n");
 # Luego guardo los IDs de las secuencias del archivo de GISAID-Mex
 
 my $ng = 0;
-my $ng_0 = 0;
-my $ng_1 = 0;
-print ("Sequences from Mexico in GISAID (file: $fileGS)\n");
-print ("note: not all these sequences will be in the Nextstrain set\n");
+print ("Sequences from Mexico downloaded from GISAID (file: $fileGS)\n");
 open (MIA, "$fileGS") or die ("Can't open file $fileGS\n");
 while (my $linea = <MIA>){
 	chomp ($linea);
 	if ($linea =~ />/){
 		if ($linea =~ />hCoV-19\/([\/\w\d-]+)\|/){
 			$gisaid{$1} = 0;
-			if (exists $nextstrain{$1}){
-				$gisaid{$1} = 1;
-				$nextstrain{$1} = 1;
-			}
+			$global{$1} = 0;
 			print ("$gisaid{$1}\t($1)\n");
 			$ng++;
-			if ($gisaid{$1} == 0){
-				$ng_0++;
-			} else {
-				$ng_1++;
-			}
 		} else {
 			die ("Pattern matching recognition (1) failed: $linea\n");
 		}
@@ -79,8 +72,6 @@ while (my $linea = <MIA>){
 }
 close (MIA);
 print ("n: $ng\n");
-print ("0: $ng_0 sequences found only in GISAID\n");
-print ("1: $ng_1 sequences found in GISAID and Nextstrain\n");
 print ("-----\n");
 #my $pausa = <STDIN>;
 
@@ -88,10 +79,10 @@ print ("-----\n");
 # Ahora veo cuales secuencias de Nextstrain faltan en GISAID-Mex
 
 my $nN = 0;
-print ("Sequences in Nextstrain latest global analysis lacking in the GISAID set\n");
+print ("Sequences in Nextstrain latest global analysis lacking in the above meixcan GISAID set\n");
 my @knextstrain = sort keys (%nextstrain);
 for (my $i = 0; $i <= $#knextstrain; $i++){
-	if ($nextstrain{$knextstrain[$i]} == 0){
+	if (!exists $gisaid{$knextstrain[$i]}){
 		print ("$nextstrain{$knextstrain[$i]}\t($knextstrain[$i])\n");
 		$nN++;
 	}
@@ -104,18 +95,35 @@ print ("-----\n");
 # Ahora veo cuales secuencias de GISAID-Mex faltan en Nextstrain
 
 my $nf = 0;
-print ("Sequences from GISAID lacking in Nextstrain latest analysis\n");
+print ("Sequences from Mexico in GISAID that are lacking in Nextstrain latest global analysis\n");
 my @kgisaid = sort keys (%gisaid);
 for (my $i = 0; $i <= $#kgisaid; $i++){
-	if ($gisaid{$kgisaid[$i]} == 0){
+	if (!exists $nextstrain{$kgisaid[$i]}){
 		print ("$gisaid{$kgisaid[$i]}\t($kgisaid[$i])\n");
 		$nf++;
 	}
 }
 print ("n: $nf\n");
-print ("note: these sequences will be extracted from $fileSe and added to outfile.fasta\n");
 print ("-----\n");
 #my $pausa = <STDIN>;
+
+#-----------------------------------------------------------------------------------------
+# Ahora veo cuales secuencias de GISAID-Mex estan tambien en Nextstrain
+
+my $nI = 0;
+print ("Sequences from GISAID also present in Nextstrain latest global analysis\n");
+my @kgisaid = sort keys (%gisaid);
+for (my $i = 0; $i <= $#kgisaid; $i++){
+	if (exists $nextstrain{$kgisaid[$i]}){
+		print ("$gisaid{$kgisaid[$i]}\t($kgisaid[$i])\n");
+		$nI++;
+		$gisaid{$kgisaid[$i]} = 1;
+	}
+}
+print ("n: $nI\n");
+print ("-----\n");
+#my $pausa = <STDIN>;
+
 
 #-----------------------------------------------------------------------------------------
 # Ahora extraigo de sequences.fasta, las secuencias de GISAID que no estan en Nextstrain
@@ -205,11 +213,12 @@ for (my $i = 0; $i <= $#keys; $i++){
 # Ahora veo cuales secuencias de GISAID-Mex me siguen faltando en Nextstrain
 
 my $nF = 0;
-print ("Sequences from GISAID not yet included in Nextstrain\n");
-print ("note: these sequences (if anay) and their metadata have to be downloaded from GISAID and included manually in outfile.fasta and outfile.txt\n");
+my %faltan;
+print ("Sequences from GISAID not yet included in Nextstrain and not present in Nextstrain latest global analysis\n");
 for (my $i = 0; $i <= $#kgisaid; $i++){
 	if ($gisaid{$kgisaid[$i]} == 0){
-		print ("$kgisaid[$i]\t$gisaid{$kgisaid[$i]}\n");
+		print ("($kgisaid[$i])\t$gisaid{$kgisaid[$i]}\n");
+		$faltan{$kgisaid[$i]} = 0;
 		$nF++;
 	}
 }
@@ -217,17 +226,127 @@ print ("n: $nF\n");
 print ("-----\n");
 #my $pausa = <STDIN>;
 
+# Ahora descargo esas secuencias de GISAID-Mex que faltan en Nextstrain
+
+$r = 0;
+$sec = ();
+$name = ();
+my $m2 = 0;
+my %meta2;
+my %sizes;
+
+open (MIA, "$fileGS") or die ("Can't open file $fileGS\n");
+open (ROB, ">outfile2.fasta") or die ("Can't open file outfile2.fasta\n");
+while (my $linea = <MIA>){
+	chomp ($linea);
+	if ($linea =~ />/ && $r == 1){
+		if ($name =~ />hCoV-19\/(.+)\|(.+)\|(.+)/){
+			#print ("probando: ($1)\n");
+			#my $pausa = <STDIN>;
+			my $nombre = $1;
+			if (exists $faltan{$nombre} && $faltan{$nombre} == 0){
+				print ROB (">$nombre\n");
+				print ROB ("$sec\n");
+				$gisaid{$nombre} = 1;
+				$meta2{$nombre} = 0;
+				$sizes{$nombre} = length($sec);
+				$m2++;
+			}
+		} else {
+			die ("Pattern matching recognition (3) failed: ($name)\n");
+		}
+		$r = 0;
+		$name = $sec = ();
+	}
+	if ($linea !~ />/ && $r == 1){
+		$sec = $sec.$linea;
+	}
+	if ($linea =~ />/ && $r == 0){
+		$name = $linea;
+		$r = 1;
+	}
+}
+if ($r == 1){
+	if ($name =~ />hCoV-19\/(.+)\|(.+)\|(.+)/){
+		my $nombre = $1;
+		if (exists $faltan{$nombre} && $faltan{$nombre} == 0){
+			print ROB (">$nombre\n");
+			print ROB ("$sec\n");
+			$gisaid{$nombre} = 1;
+			$meta2{$nombre} = 0;
+			$sizes{$nombre} = length($sec);
+			$m2++;
+		}
+	} else {
+		die ("Pattern matching recognition (3) failed: ($name)\n");
+	}
+	$r = 0;
+	$name = $sec = ();
+}
+close (ROB);
+close (MIA);
+
+# Ahora extraigo los metadatos
+
+my $l = 0;
+open (MIA, "$fileM2") or die ("Can't open file $fileM2\n");
+open (ROB, ">outfile2.txt") or die ("Can't open file outfile2.txt\n");
+# La siguiente linea es solo por control:
+# print("strain\tvirus\tgisaid_epi_isl\tgenbank_accession\tdate\tregion\tcountry\tdivision\tlocation\tregion_exposure\tcountry_exposure\tdivision_exposure\tsegment\tlength\thost\tage\tsex\tNextstrain_clade\tpangolin_lineage\tGISAID_clade\toriginating_lab\tsubmitting_lab\tauthors\turl\ttitle\tpaper_url\tdate_submitted\tpurpose_of_sequencing\n");
+while (my $linea = <MIA>){
+	chomp ($linea);
+	my @a = split (/\t/, $linea);
+	$l++;
+	if ($l == 1){
+		# print ROB ("$linea\n");
+	} else {
+		$a[0] =~ s/hCoV-19\///;
+		if (exists $meta2{$a[0]}){
+			# print ROB ("$linea\n");
+			my @r = split (/ \/ /, $a[3]);
+			print ("check regions and size: ($r[0])\t($r[1])\t($r[2])\t($sizes{$a[0]})\n");
+			print ROB ("$a[0]\tncov\t$a[1]\t?\t$a[2]\t$r[0]\t$r[1]\t$r[2]\t\t\t\t\tgenome\t$sizes{$a[0]}\t$a[4]\t$a[7]\t$a[6]\t?\t$a[12]\t$a[13]\t?\t?\t?\t?\t?\t?\t$a[2]\t?\n");
+			$meta2{$a[0]} = 1;
+		} else {
+			#print ("Warning, the sequence ($a[0]) does not exist in $fileM2\n");
+		}
+	}
+}
+close (ROB);
+close (MIA);
+my @keys = sort keys (%meta2);
+for (my $i = 0; $i <= $#keys; $i++){
+	if ($meta2{$keys[$i]} == 0){
+		print ("there is no metadata for the sequence: $keys[$i]\t($meta2{$keys[$i]})\n");
+	}
+}
+print ("-----\n");
+
 #-----------------------------------------------------------------------------------------
 # Ahora hago un reporte
 
-print ("Sequences from Mexico in Nextstrain .......................................: $ns\n");
-print ("Sequences from Mexico in GISAID ...........................................: $ng\n");
-print ("Sequences from Mexico in GISAID that are absent in Nextstrain .............: $nf\n");
-print ("Sequences from Mexico in GISAID that are absent in Nextstrain, recovered ..: $m\n");
-print ("Sequences from Mexico in Nextstrain absent from GISAID (not yet formated)..: $nN\n");
-print ("Sequences from Mexico in GISAID that continue to be absent from Nextstrain : $nF\n");
+print ("Sequences from Mexico in Nextstrain latest global analysis (LGA) .................................................: $ns\n");
+print ("Sequences from Mexico in GISAID (including those with and without Nextstrain format) .............................: $ng\n");
+print ("Sequences shared between the above sets ..........................................................................: $nI\n");
+print ("Sequences from Mexico in Nextstrain LGA that are absent from the mexican GISAID set ..............................: $nN\n");
+print ("Sequences from Mexico in GISAID that are absent in Nextstrain LGA ................................................: $nf\n");
+my $tot = $nI + $nN + $nf;
+print ("Total sequences from Mexico ......................................................................................: $tot\n");
+print ("-----\n");
+print ("Sequences from Mexico in GISAID that are absent in Nextstrain LGA and were already formated for Nextstrain .......: $m\n");
+print ("Sequences from Mexico in GISAID that are absent in Nextstrain LGA that do not have the format for Nextstrain yet .: $nF:$m2\n");
+if ($nf == ($m+$nF)){
+	print ("$nf = ($m + $nF), looks Ok!\n");
+} else {
+	print ("$nf != ($m + $nF), warning!\n");
+}
+if ($nF == $m2){
+	print ("$nF = $m2, looks Ok!\n");
+} else {
+	print ("$nF != $m2, warning!\n");
+}
 print ("-----\n");
 print ("Now please cat the following files (example):\n");
-print ("\$ cat nextstrain_ncov_global_metadata.f.tsv outfile.txt > nextstrain_ncov_global_metadata.fc.tsv\n");
-print ("\$ cat nextstrain_ncov_global_metadata.fasta outfile.fasta > nextstrain_ncov_global_metadata.fc.fasta\n");
+print ("\$ cat nextstrain_ncov_global_metadata.f.tsv outfile.txt outfile2.txt > nextstrain_ncov_global_metadata.fc.tsv\n");
+print ("\$ cat nextstrain_ncov_global_metadata.fasta outfile.fasta outfile2.fasta > nextstrain_ncov_global_metadata.fc.fasta\n");
 print ("-----\n");
