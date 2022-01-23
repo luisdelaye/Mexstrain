b55a05a1adbfaea7a2f0e95cd4222bc391bc9443
Luis Delaye <luis.delaye@cinvestav.mx>
2022-01-23 17:51:09 -0600

    Add files via upload

---
+++ select_sequences_to_include.pl
@@ -0,0 +1,39 @@
+#!/usr/local/perl
+
+# This script extracts IDs from a fasta file.
+
+# use
+# perl select_sequences_to_include.pl sequences.sampled.fasta Mexico > add_to_the_include_file.txt
+
+# See https://github.com/luisdelaye/Mexstrain/ for more details.
+
+# Author
+# Luis Jose Delaye Arredondo
+# Laboratorio de Genomica Evolutiva
+# Departamento de Ingenieria Genetica
+# CINVESTAV - Irapuato
+# Mexico
+# luis.delaye@cinvestav.mx
+# Copy-Left  = : - )
+
+# beta.1.0 version
+
+use strict;
+#-------------------------------------------------------------------------------
+# Declare global variables
+my $file = $ARGV[0];
+my $country = $ARGV[1];
+
+#-------------------------------------------------------------------------------
+# Extract the names of the sequences
+
+open (MIA, "$file") or die ("Can't open $file\n");
+while (my $linea = <MIA>){
+	chomp ($linea);
+	if ($linea =~ /\/$country\//){
+		$linea =~ s/>//;
+		print ("$linea\n");
+	}
+}
+close (MIA);
+#-------------------------------------------------------------------------------
