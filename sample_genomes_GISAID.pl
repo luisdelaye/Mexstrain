50509792c72c22601009dee4bed923f1c4cc44f1
Luis Delaye <luis.delaye@cinvestav.mx>
2022-04-06 20:20:52 -0500

    Update sample_genomes_GISAID.pl

--- sample_genomes_GISAID.pl
+++ sample_genomes_GISAID.pl
@@ -7,8 +7,9 @@
 # out:
 # outfile.tsv
 
-# 'seed' is an integer used to generate random numbers. Example: 2718 and
-# 'n' is the maximum number of genomes to sample per Pango lineage per month.
+# 'seed' is an integer used to generate random numbers. Example: 2718
+# 'n' is the percentage of genomes to sample per Pango lineage per month
+
 
 # See https://github.com/luisdelaye/Mexstrain/ for more details.
 
@@ -27,9 +28,11 @@ use strict;
 #-------------------------------------------------------------------------------
 # Global variables
 
-my $file   = $ARGV[0]; # gisaid_hcov-19_2021_##_##.e1.tsv file
-my $seed   = $ARGV[1]; # random number
-my $rounds = $ARGV[2]; # rounds of selection
+my $file     = $ARGV[0]; # gisaid_hcov-19_2021_##_##.e1.tsv file
+my $seed     = $ARGV[1]; # random number
+my $rounds   = $ARGV[2]; # 'n'
+
+my $strategy = 1;
 
 srand($seed);
 
@@ -84,7 +87,7 @@ my @kclade = sort keys (%clade);
 print ("\n------------------------------------------------------------------------\n");
 print ("Number of genomes per collection date\n\n");
 for (my $i = 0; $i <= $#kdates; $i++){
-  print ("$kdates[$i]\t$dates{$kdates[$i]}\n");
+	print ("$kdates[$i]\t$dates{$kdates[$i]}\n");
 }
 print ("\n------------------------------------------------------------------------\n");
 print ("Number of genomes per Pangolin lineage\n\n");
@@ -101,44 +104,88 @@ for (my $i = 0; $i <= $#kclade; $i++){
 print ("\n------------------------------------------------------------------------\n");
 print ("Sampling genomes, per Pango linage per month\n\n");
 for (my $i = 0; $i <= $#kdates; $i++){
-  print ("$kdates[$i]\t$dates{$kdates[$i]}\n");
+	print ("---------------------------------------------------------\n");
+	print ("---------------------------------------------------------\n");
+  print ("Date.............: $kdates[$i]\n");
+	print ("Number of genomes: $dates{$kdates[$i]}\n");
+
   for (my $j = 0; $j <= $#kpango; $j++){
     if (exists $hashA->{$kdates[$i]}{$kpango[$j]}){
-      print ("\t$kpango[$j]\n");
-      #for (my $k = 0; $k <= $#{$hashA->{$kdates[$i]}{$kpango[$j]}}; $k++){
-      #  if ($k < $#{$hashA->{$kdates[$i]}{$kpango[$j]}}){
-      #    print ("${$hashA->{$kdates[$i]}{$kpango[$j]}}[$k]; ");
-      #  } else {
-      #    print ("${$hashA->{$kdates[$i]}{$kpango[$j]}}[$k]\n");
-      #  }
-      #}
-      my $s = 0;
-      CICLOA:
-      while ($s < $rounds){
-        my $retval = int(rand($#{$hashA->{$kdates[$i]}{$kpango[$j]}} +1));
-        #print ("\t\tselected (index: $retval; N: $N) -> (${$hashA->{$kdates[$i]}{$kpango[$j]}}[$retval])\n");
-				print ("\t\tsampled -> (${$hashA->{$kdates[$i]}{$kpango[$j]}}[$retval])\n");
-        if (!exists $selected{${$hashA->{$kdates[$i]}{$kpango[$j]}}[$retval]}){
-          $selected{${$hashA->{$kdates[$i]}{$kpango[$j]}}[$retval]} = 1;
-          push (@selected, ${$hashA->{$kdates[$i]}{$kpango[$j]}}[$retval]);
-          $s++;
-          my @tmp = @{$hashA->{$kdates[$i]}{$kpango[$j]}};
-          my $idt = ${$hashA->{$kdates[$i]}{$kpango[$j]}}[$retval];
-          @{$hashA->{$kdates[$i]}{$kpango[$j]}} = ();
-          for (my $k = 0; $k <= $#tmp; $k++){
-            if ($tmp[$k] !~ /$idt/){
-              push (@{$hashA->{$kdates[$i]}{$kpango[$j]}}, $tmp[$k]);
-            }
-          }
+			print ("\n---------------------------------------------------------\n");
+      print ("Pango lineage:\t$kpango[$j]\n");
+			print ("Genomes:\n");
+      for (my $k = 0; $k <= $#{$hashA->{$kdates[$i]}{$kpango[$j]}}; $k++){
+        if ($k < $#{$hashA->{$kdates[$i]}{$kpango[$j]}}){
+          print ("${$hashA->{$kdates[$i]}{$kpango[$j]}}[$k]; ");
         } else {
-          print ("this genome was already selected: ${$hashA->{$kdates[$i]}{$kpango[$j]}}[$retval]}\n");
-					#die ("run again the script but using a different seed for the random number generator\n");
-        }
-        my $asize = @{$hashA->{$kdates[$i]}{$kpango[$j]}};
-        if ($asize == 0){
-          last (CICLOA);
+          print ("${$hashA->{$kdates[$i]}{$kpango[$j]}}[$k]\n");
         }
       }
+			my $Ng = $#{$hashA->{$kdates[$i]}{$kpango[$j]}} +1;
+			if ($strategy == 1){
+				my $treshold = $Ng*$rounds/100;
+				my $redondeado = int($treshold + 0.5);
+				my $s = 1;
+				if ($s <= $redondeado){
+					print ("Sampled genomes:\n")
+				}
+				CICLOA:
+				while ($s <= $redondeado){
+					my $retval = int(rand($#{$hashA->{$kdates[$i]}{$kpango[$j]}} +1));
+					#print ("\t\tselected (index: $retval; N: $N) -> (${$hashA->{$kdates[$i]}{$kpango[$j]}}[$retval])\n");
+					print ("${$hashA->{$kdates[$i]}{$kpango[$j]}}[$retval]\n");
+					if (!exists $selected{${$hashA->{$kdates[$i]}{$kpango[$j]}}[$retval]}){
+						$selected{${$hashA->{$kdates[$i]}{$kpango[$j]}}[$retval]} = 1;
+						push (@selected, ${$hashA->{$kdates[$i]}{$kpango[$j]}}[$retval]);
+						$s++;
+						my @tmp = @{$hashA->{$kdates[$i]}{$kpango[$j]}};
+						my $idt = ${$hashA->{$kdates[$i]}{$kpango[$j]}}[$retval];
+						@{$hashA->{$kdates[$i]}{$kpango[$j]}} = ();
+						for (my $k = 0; $k <= $#tmp; $k++){
+							if ($tmp[$k] !~ /$idt/){
+								push (@{$hashA->{$kdates[$i]}{$kpango[$j]}}, $tmp[$k]);
+							}
+						}
+					} else {
+						print ("this genome was already selected: ${$hashA->{$kdates[$i]}{$kpango[$j]}}[$retval]}\n");
+						#die ("run again the script but using a different seed for the random number generator\n");
+					}
+					my $asize = @{$hashA->{$kdates[$i]}{$kpango[$j]}};
+					if ($asize == 0){
+						last (CICLOA);
+					}
+				}
+			} elsif ($strategy == 0){
+      		my $s = 0;
+      		CICLOA:
+      		while ($s < $rounds){
+        		my $retval = int(rand($#{$hashA->{$kdates[$i]}{$kpango[$j]}} +1));
+        		#print ("\t\tselected (index: $retval; N: $N) -> (${$hashA->{$kdates[$i]}{$kpango[$j]}}[$retval])\n");
+						print ("\t\tsampled -> (${$hashA->{$kdates[$i]}{$kpango[$j]}}[$retval])\n");
+        		if (!exists $selected{${$hashA->{$kdates[$i]}{$kpango[$j]}}[$retval]}){
+          		$selected{${$hashA->{$kdates[$i]}{$kpango[$j]}}[$retval]} = 1;
+          		push (@selected, ${$hashA->{$kdates[$i]}{$kpango[$j]}}[$retval]);
+          		$s++;
+          		my @tmp = @{$hashA->{$kdates[$i]}{$kpango[$j]}};
+          		my $idt = ${$hashA->{$kdates[$i]}{$kpango[$j]}}[$retval];
+          		@{$hashA->{$kdates[$i]}{$kpango[$j]}} = ();
+          		for (my $k = 0; $k <= $#tmp; $k++){
+            		if ($tmp[$k] !~ /$idt/){
+              		push (@{$hashA->{$kdates[$i]}{$kpango[$j]}}, $tmp[$k]);
+            		}
+          		}
+        		} else {
+          		print ("this genome was already selected: ${$hashA->{$kdates[$i]}{$kpango[$j]}}[$retval]}\n");
+							#die ("run again the script but using a different seed for the random number generator\n");
+        		}
+        		my $asize = @{$hashA->{$kdates[$i]}{$kpango[$j]}};
+        		if ($asize == 0){
+          		last (CICLOA);
+        		}
+      		}
+			} else {
+				die ("Please use 0 or 1 as the last parameter when running the script\n");
+			}
     }
   }
 }
@@ -152,5 +199,4 @@ print ("\n");
 print ("------------------------------------------------------------------------\n");
 print ("Number of genomes sampled: $N\n");
 print ("------------------------------------------------------------------------\n");
-#die ("bien!\n");
 #-------------------------------------------------------------------------------
